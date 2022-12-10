515f019cb690a233457d32ea997575e1a22f4a63
Yann Savary <yann@winds.mobi>
2022-12-10 17:55:52 +0100

    Possibility to disable opentelemetry with env variable TELEMETRY_DISABLED

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,3 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-opentelemetry-instrument --traces_exporter otlp uvicorn --proxy-headers --root-path "$ROOT_PATH" --host 0.0.0.0 --port "$PORT" winds_mobi_api.main:app
+if [[ $TELEMETRY_DISABLED ]]; then
+  uvicorn --proxy-headers --root-path "$ROOT_PATH" --host 0.0.0.0 --port "$PORT" winds_mobi_api.main:app
+else
+  opentelemetry-instrument --traces_exporter otlp uvicorn --proxy-headers --root-path "$ROOT_PATH" --host 0.0.0.0 --port "$PORT" winds_mobi_api.main:app
+fi
