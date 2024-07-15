1f7944288a1eff23ff22e25b040923490ffc4904
Yann Savary <yann@winds.mobi>
2024-07-16 00:20:02 +0200

    Inject database singleton with fastapi injection

--- winds_mobi_api/database.py
+++ winds_mobi_api/database.py
@@ -1,10 +1,22 @@
-_mongodb = None
-_mongodb_sync = None
+from threading import local
+
+from motor import motor_asyncio
+from pymongo import MongoClient
+
+from winds_mobi_api.settings import settings
+
+motor_database = None
+thread_local = local()
 
 
 def mongodb():
-    return _mongodb
+    global motor_database
+    if motor_database is None:
+        motor_database = motor_asyncio.AsyncIOMotorClient(settings.mongodb_url).get_database()
+    return motor_database
 
 
 def mongodb_sync():
-    return _mongodb_sync
+    if not hasattr(thread_local, "mongodb_database"):
+        thread_local.mongodb_database = MongoClient(settings.mongodb_url).get_database()
+    return thread_local.mongodb_database
